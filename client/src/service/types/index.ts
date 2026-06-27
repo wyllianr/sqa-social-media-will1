a74130379d07ed1bc96fc5cdfae1641f7fc4f659
wyllianr <wyribiski@gmail.com>
2026-06-26 21:10:19 -0300

    Commit 1 - Atividade 6: Likes e dislikes + DummyJson

--- client/src/service/types/index.ts
+++ client/src/service/types/index.ts
@@ -27,6 +27,10 @@ export interface Post {
   title: string;
   body: string;
   liked: boolean;
+  reactions?: {
+    likes: number;
+    dislikes: number;
+  };
 }
 
 export interface PostsResponse {
